6bc40feee7f6033f8f2e498471823dee7ef54d1d
Andrew Hollis <thejester129@gmail.com>
2023-03-08 10:43:06 +0000

    gotcha 1

---
+++ chaining-gotcha1.js
@@ -0,0 +1,44 @@
+// wrong
+let aThenB = (promise1, promise2) => {
+  promise1.then(() => promise2);
+};
+
+const promise1 = new Promise((resolve) =>
+  setTimeout(() => {
+    console.log("1");
+    resolve();
+  }, 500)
+);
+
+aThenB(
+  promise1,
+  new Promise((resolve) =>
+    setTimeout(() => {
+      console.log("...then 2");
+      resolve();
+    }, 100)
+  )
+);
+
+// correct
+aThenB = (promise1, promise2) => {
+  promise1.then(() => promise2());
+};
+
+const promise3 = new Promise((resolve) =>
+  setTimeout(() => {
+    console.log("3");
+    resolve();
+  }, 500)
+);
+
+aThenB(
+  promise3,
+  () =>
+    new Promise((resolve) =>
+      setTimeout(() => {
+        console.log("...then 4");
+        resolve();
+      }, 100)
+    )
+);
